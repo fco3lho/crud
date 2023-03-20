0379f1bde62f24ba585b60479fa743872746a758
Felipe Campos <felipecampos50123@gmail.com>
2023-03-20 16:19:58 -0300

    Criando formulário

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,53 @@
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [values, setValues] = useState();
+
+  // Salva os dados alterados no formulário.
+  const handleChangeValues = (e) => {
+    setValues((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  // Envia os dados do formulário ao clicar no botão.
+  const handleSubmit = (e) => {
+    console.log(values);
+  };
+
   return (
-    <div className="App">
-      Right
+    <div className="app-container">
+      <div className="register-container">
+        <h1>Lojinha do Felp</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register-input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          name="cost"
+          placeholder="Preço"
+          className="register-input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Categoria"
+          className="register-input"
+          onChange={handleChangeValues}
+        />
+
+        <button className="register-button" onClick={() => handleSubmit()}>
+          Cadastrar
+        </button>
+      </div>
     </div>
   );
 }
