c6ead144b9ce3e316d7bc74e15195ff33112a1a6
Felipe Campos <felipecampos50123@gmail.com>
2023-03-23 14:56:48 -0300

    Tentando conectar ao banco de dados

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const database = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "",
+});
 
 app.get("/", (request, result) => {
   result.send("Hello world");
+  //let SQL = "";
 });
 
 app.listen(3001, () => {
